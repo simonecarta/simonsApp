fad05825ab87a4898ebfb74a6afbe870d4eb9f8d
simonecarta <simonecartaolbia@gmail.com>
2023-03-28 22:00:22 +0200

    Second milestone started - Autoreply

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ $(document).ready(function () {
     var microphoneIcon = $("i.fa-solid.fa-microphone.fa-lg")
     var sendIcon = $("i.fa-sharp.fa-solid.fa-paper-plane.fa-lg")
 
+    var inputSearchContact = $(".newChatSearch")
+
 
     // changing icon when input's value changes
    inputMessage.keyup(function(){
-    if ( inputMessageValue != "") {
+    if ( inputMessageValue !== "") {
         microphoneIcon.addClass("nv");
         sendIcon.removeClass("nv")
     } else if (inputMessageValue === " "){
@@ -24,7 +26,6 @@ $(document).ready(function () {
         sendMessage(inputMessage);
     })
 
-
     //with enter 
     inputMessage.keypress(function(e) {
         if(e.which == 13){
@@ -32,12 +33,15 @@ $(document).ready(function () {
         }
     })
 
+    // search a contact in a list 
+    
+    searchContact(inputSearchContact)
+
 
 
     /**
      * FUNCTIONS 
      */
-
 
     function sendMessage(input){
         // ottieni testo
@@ -54,8 +58,8 @@ $(document).ready(function () {
 
             //creazione e inserimento ora attuale
             var data = new Date();
-            var ora = data.getHours();
-            var minuti = data.getMinutes();
+            var ora = addZero(data.getHours());
+            var minuti = addZero(data.getMinutes());
             var orario = ora + ":" + minuti;
             nuovoMessaggio.children('.message-time').text(orario)
 
@@ -65,7 +69,44 @@ $(document).ready(function () {
             // reset input messaggio 
             inputMessage.val("")
 
+            // risposta automatica utente 
+            autoReply()
     }}
 
+
+    function addZero (numero){
+        if(numero < 10){
+            numero = "0" + numero
+        }
+        return numero
+    }
+
+
+    function autoReply (){
+
+        setTimeout(function(){
+
+            // clonazione del messaggio
+            var autoResponse = $(".template .received").clone();
+
+            // aggiunta del testo al messaggio
+            autoResponse.children('.message-text').text("Questa è una risposta automatica");
+            
+            // aggiunta dell'ora attuale alla risposta
+            var data = new Date();
+            var ora = addZero(data.getHours());
+            var minuti = addZero(data.getMinutes());
+            var orario = ora + ":" + minuti;
+
+            autoResponse.children('.message-time').text(orario)
+
+            // aggiunta nuovo messaggio al contenitore attivo
+            $(".right-messages.active").append(autoResponse)
+
+        },1000)
+
+    }
+
+        
 
 }); //end doc ready
